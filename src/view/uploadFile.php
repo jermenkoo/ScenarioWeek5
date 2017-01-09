5afddae1d641d472a4720b2ee9911310c175f1d7
Martí Serra <martiserravivancos@gmail.com>
2017-01-09 18:13:21 +0000

    Uploading files

--- src/view/uploadFile.php
+++ src/view/uploadFile.php
@@ -9,7 +9,7 @@
     ?>
 
 
-    <form action="upload.php" method="post" enctype="multipart/form-data">
+    <form action="../logic/upload.php" method="post" enctype="multipart/form-data">
         Select file to upload:
         <input type="file" name="fileToUpload" id="fileToUpload">
         <input type="submit" value="Upload Image" name="submit">
