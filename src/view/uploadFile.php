d480102296d163b9a7c46974fb380ce85343e41e
Martí Serra <martiserravivancos@gmail.com>
2017-01-09 18:54:58 +0000

    Form improvements

--- src/view/uploadFile.php
+++ src/view/uploadFile.php
@@ -3,19 +3,15 @@
         <link rel="stylesheet" href="../styles/style.css">
     </head>
     <body>
-
-    <?php
-    include 'header.php';
-    ?>
-
-
-    <form action="../logic/upload.php" method="post" enctype="multipart/form-data">
-        Select file to upload:
-        <input type="file" name="fileToUpload" id="fileToUpload">
-        <input type="submit" value="Upload Image" name="submit">
-    </form>
+        <?php
+        include 'header.php';
+        ?>
 
 
-
+        <form action="../logic/upload.php" method="post" enctype="multipart/form-data">
+            Select file to upload:
+            <input type="file" name="fileToUpload" id="fileToUpload">
+            <input type="submit" value="Upload file" name="submit">
+        </form>
     </body>
 </html>
