79677407c3026f733cf2e55e03fe2bf8725d1e32
Janos Potecki <janospotecki@Janoss-MacBook-Pro.local>
2017-01-10 12:44:53 +0000

    index.php doesn't show warnings anymore and displays users and last snippets

--- src/view/publicUsersTable.php
+++ src/view/publicUsersTable.php
@@ -3,8 +3,12 @@
 <?php
 $users = getAllUsers();
        foreach ($users as $user){
+           $snippet = getPublicSnippet($user['id']);
+           if ($snippet == NULL) {
+               $snippet = "No snippet";
+           }
            echo '<tr>';
-           echo '<td>' . $user['username'] . '</td><td>' . $user['id'] . '</td><td>' . $user['snippet'] . '</td>';
+           echo '<td>' . $user['username'] . '</td><td>' . $user['id'] . '</td><td>' . $snippet . '</td>';
            echo '</tr>';
        }
 ?>
