3eb3cb96eafa788dddbf4fc825378f8a85731f4b
Martí Serra <martiserravivancos@gmail.com>
2017-01-10 12:17:23 +0000

    Added upload file button in header when logged in

--- src/view/header.php
+++ src/view/header.php
@@ -2,12 +2,13 @@
     <?php 
         if (isset($_GET['username']) && isset($_GET['pw']) && loggedIn($_GET['username'], $_GET['pw'])) {
             echo "<div class='nav-buttons'>",
+                "<a href='/src/view/uploadFile.php'><span class='user-button'>Upload file</span></a>",
                 $_GET['username'],
                 "</div>"; 
         } else {
             echo "<div class='nav-buttons'>",
-                "<a href='/src/view/login.php'><span class='nav-button'>Log in</span></a>",
-                "<a href='/src/view/signup.php'><span class='nav-button'>Sign up</span></a>",
+                "<a href='/src/view/login.php'><span class='user-button'>Log in</span></a>",
+                "<a href='/src/view/signup.php'><span class='user-button'>Sign up</span></a>",
                 "</div>";
         }
     ?>
