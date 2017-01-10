aabac219b5618923d61ad1e512c2e9364a06322c
Martí Serra <martiserravivancos@gmail.com>
2017-01-10 11:52:41 +0000

    Sign up

--- src/view/forms/signupForm.php
+++ src/view/forms/signupForm.php
@@ -1,5 +1,5 @@
 <form action="/index.php" method="get"> 
-    Login Name </br>
+    User Name </br>
     <input type="text" name="username"></br>
     Password </br>
     <input type="password" name="pw"></br>
