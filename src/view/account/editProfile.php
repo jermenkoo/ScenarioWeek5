ef79b201dcf7ae95605fbc5dab8a24525938b2a1
Martí Serra <martiserravivancos@gmail.com>
2017-01-11 16:18:20 +0000

    UI

--- src/view/account/editProfile.php
+++ src/view/account/editProfile.php
@@ -10,7 +10,9 @@
 
     // User already logged in
     if (isset($_COOKIE['user']) && isset($_COOKIE['pw'])) {
+      echo "<div class='file-upload'>";
       include '../forms/editProfileForm.php';
+      echo "</div>";
     } else {
       header('Location: ' . 'http://' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . '/index.php');
     }
