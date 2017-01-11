0aa39e8543b2f02d43e4e7df91bea8a50453dafd
Martí Serra <martiserravivancos@gmail.com>
2017-01-11 16:11:33 +0000

    Improved file upload

--- src/view/uploadFile.php
+++ src/view/uploadFile.php
@@ -19,6 +19,17 @@
         ?>
 
         <form class="file-upload" action="../logic/upload.php" method="post" enctype="multipart/form-data">
+            <?php
+            if (isset($_GET['url'])) {
+              $host = 'http://' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . '/';
+              echo "<div>Your file is available here: " . $host . $_GET['url'] . "</div>";
+              echo "<br>";
+            } elseif (isset($_GET['error'])) {
+              echo "<div>" . $_GET['error'] . "</div>";
+              echo "<br>";
+            }
+            ?>
+
             Select file to upload:
             <input type="file" name="fileToUpload" id="fileToUpload">
             <input type="submit" value="Upload file" name="submit">
