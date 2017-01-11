818674373cec81e335f5713ad761adc7ad364899
Martí Serra <martiserravivancos@gmail.com>
2017-01-11 12:37:47 +0000

    All snippets button in home

--- src/view/publicUsersTable.php
+++ src/view/publicUsersTable.php
@@ -10,7 +10,9 @@
                 $snippet = "No snippet";
             }
             echo '<div class="snippet-container">';
-            echo '<div>' . $colouredUser . '</div><div>' . $snippet . '</div>';
+            echo '<span class="user-label">' . $colouredUser . '</span>'
+            . '<a href="/src/view/snippets.php?userId=' . $user['id'] . '"><span>All snippets</span></a>'
+            . '<div>' . $snippet . '</div>';
             echo '</div>';
         }
     ?>
