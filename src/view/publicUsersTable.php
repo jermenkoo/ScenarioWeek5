2d457b8ed0f4dae608194da2ed2beccf05619699
Martí Serra <martiserravivancos@gmail.com>
2017-01-12 13:40:45 +0000

    Scaping name in home

--- src/view/publicUsersTable.php
+++ src/view/publicUsersTable.php
@@ -13,12 +13,20 @@
             $iconUrl = getIcon($user['id']);
             echo '<div class="user">';
             echo "<img class='user-image' src='" . $iconUrl . "' />";
-            echo '<span class="user-label">' . $colouredUser . '</span>'
-            . '<a href="/src/view/snippets.php?userId=' . $user['id'] . "&userName=" . $user['username'] . '"><span>All snippets</span></a>';
+            echo '<span class="user-label">'; ?>
+
+            <script type="text/javascript">
+              var user = "<?php echo $colouredUser; ?>";
+              document.write(DOMPurify.sanitize(user));
+            </script>
+
+            <?php
+            echo '</span>' . '<a href="/src/view/snippets.php?userId=' . $user['id'] .
+            "&userName=" . $user['username'] . '"><span>All snippets</span></a>';
+
             if ($user['homepage'] && isset($_COOKIE['user']) && validCredentials($_COOKIE['user'], $_COOKIE['pw'])) {
               echo '&nbsp;<a href="' . $user['homepage'] .  '">Homepage</a>';
             }
-
             ?>
 
             </div>
@@ -28,7 +36,6 @@
                 <script type="text/javascript">
                   var snip = "<?php echo $snippet; ?>";
                   var user = "<?php echo $colouredUser; ?>";
-                  console.log(snip);
                   document.write(DOMPurify.sanitize(snip));
                   document.querySelector('.user-label').innerHTML = DOMPurify.sanitize(user);
                 </script>
