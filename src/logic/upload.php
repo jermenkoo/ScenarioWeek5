0434f3ec887c6a07e29ee88d6c563ba1b8a1a6d2
Martí Serra <martiserravivancos@gmail.com>
2017-01-13 22:14:00 +0000

    Upload not working

--- src/logic/upload.php
+++ src/logic/upload.php
@@ -8,35 +8,29 @@ if (!file_exists($target_dir)) {
 }
 
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 
 // Check if file already exists
 if (file_exists($target_file)) {
     header('Location: ' . $URL . '/src/view/uploadFile.php?error=' . "Sorry, file already exists.");
-    $uploadOk = 0;
-}
-
-if ($_FILES["fileToUpload"]["type"] != "image/jpeg") {
-    header('Location: ' . $URL . '/src/view/uploadFile.php?error=' . "File is not an image!");
     die();
-    $uploadOk = 0;
 }
 
-if ( ($_FILES["fileToUpload"]["size"] > 350000) || ($_FILES["fileToUpload"]["size"] < 10000) ) {
-    header('Location: ' . $URL . '/src/view/uploadFile.php?error=' . "File is too small or too large!");
-    $uploadOk = 0;
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+  header('Location: ' . $URL . '/src/view/uploadFile.php?error=' . "File is not an image!");
+  die();
 }
 
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
+if ($_FILES["fileToUpload"]["size"] > 350000) {
+    header('Location: ' . $URL . '/src/view/uploadFile.php?error=' . "File is too large!");
+    die();
+}
+
+if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    $fileurl = '/userID_' . $_SESSION['userID'] . "/" . basename($_FILES["fileToUpload"]["name"]);
+    header('Location: ' . $URL . '/src/view/uploadFile.php?url=' . $fileurl);
 } else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        $fileurl = '/userID_' . $_SESSION['userID'] . "/" . basename($_FILES["fileToUpload"]["name"]);
-        header('Location: ' . $URL . '/src/view/uploadFile.php?url=' . $fileurl);
-    } else {
-      header('Location: ' . $URL . '/src/view/uploadFile.php?error=' . "Sorry, there was an error uploading your file.");
-    }
+  header('Location: ' . $URL . '/src/view/uploadFile.php?error=' . "Sorry, there was an error uploading your file.");
 }
 ?>
